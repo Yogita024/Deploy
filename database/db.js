a1f58ede03dab602458697eb023b3aac85dbeddd
yadavyogita <yadusince2004@gmail.com>
2024-01-13 18:12:32 +0530

    changed

--- database/db.js
+++ database/db.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv';
 
 const Connection =()=>
 {   dotenv.config()
-    const DB_URI =`mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@ac-2wcjthi-shard-00-00.l3kw7sf.mongodb.net:27017,ac-2wcjthi-shard-00-01.l3kw7sf.mongodb.net:27017,ac-2wcjthi-shard-00-02.l3kw7sf.mongodb.net:27017/?ssl=true&replicaSet=atlas-gl0069-shard-0&authSource=admin&retryWrites=true&w=majority`;
+  
+    const DB_URI =`mongodb://${USERNAME}:${PASSWORD}';
     try{
            mongoose.connect(DB_URI,{ useNewUrlParser: true });
            console.log('Database connected succesfully');
